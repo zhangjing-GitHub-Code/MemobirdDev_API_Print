eceb637036c851332cc0c5edb60d093efb194c93
zhangjing <zhangjing20091105@126.com>
2021-04-03 14:47:01 +0800

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 #include<fstream>
 using namespace std;
 string getExeStr() {
-	string _exe = "https://open.memobird.cn/home/printpaper?";
+	string _exe = "https://open.memobird.cn/home/printpaper?ak=";
+	ifstream conf("Memobird.ini");
+	string ak,uid,devid;
+	try{
+		getline(conf,ak);
+		getline(conf,uid);
+		getline(conf,devid);
+	}catch(exception){
+		cerr << "config file wrong, please check your format!";
+	}
+	_exe += ak;
+	_exe += "&userID=";
+	_exe += uid;
+	_exe += "&memobirdID=";
+	_exe += devid;
+	conf.close();
+	return _exe;
+}
+int main(){
+	string webv=getExeStr();
+	system("pause");
+	return 0;
 }
