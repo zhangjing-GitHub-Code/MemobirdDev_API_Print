38d0972ed41679a82b2ecba2489b87a465a02761
zhangjing-GitHub-Code <zhangjing20091105@126.com>
2021-09-14 20:52:29 +0800

    python version update: add colorful prompt(some termial may not support)

--- python_ver/main.py
+++ python_ver/main.py
@@ -11,7 +11,7 @@ def getconf():
         ak=s2[0]
         devid=s2[1]
         uid=s2[2]
-        print(s2)
+        #print(s2)
     except:
         raise RuntimeError("config file wrong, please check your format!")
     exe += ak
@@ -20,6 +20,10 @@ def getconf():
     exe += "\\&memobirdID\\="
     exe += devid
     return exe
+
+
+step_status="\033[32m[\033[34m*\033[32m]\033[0m "
+error_status="\033[32m[\033[31m!\033[32m]\033[0m "
 
 def main():
 # The base64 encoding depends on linux command 'base64'
@@ -30,15 +34,24 @@ def main():
     enced=base64.b64encode(unenc.encode('gbk'))
     exe+="\\&timestamp\\=2021-9-13%207:16:10"
     exe+="\\&printcontent\\=T:"+enced.decode()
-    print("\033[32m[\033[34m*\033[32m] \033[0mRequesting print url...")
-    json_s=os.popen("curl -fsS "+exe).read() 
-    #'{"showapi_res_code":1,"showapi_res_error":"ok","result":2,"smartGuid":"25eccfd3cc4061dc","printcontentid":53576399}'
-    st=json.loads(json_s)
-    if st["showapi_res_code"]==1:
-        print("\033[32m[\033[34m*\033[32m]\033[0m Print sucessfully!")
-    else : 
-        print("\033[32m[\033[31m*\033[32m]\033[0m Some thing went wrong...",end=' ErrMsg:'+st["showapi_res_error"]+"\n")
-        #print(st["showapi_res_error"]+"\n")
-        exit(1)
+    jso=os.popen("termux-dialog confirm -t \"Do you really want to print?\" -i \"1000 API calls per day and %s left\"").read()
+    jso_st=json.loads(jso)
+    if jso_st["text"]=="no":
+        print(error_status+"Aborted.")
+        exit(0)
+    try:
+        print("\033[32m[\033[34m*\033[32m] \033[0mRequesting print url...")
+        json_s=os.popen("curl -fsS "+exe).read() 
+        #'{"showapi_res_code":1,"showapi_res_error":"ok","result":2,"smartGuid":"25eccfd3cc4061dc","printcontentid":53576399}'
+        st=json.loads(json_s)
+        if st["showapi_res_code"]==1:
+            print("\033[32m[\033[34m*\033[32m]\033[0m Print sucessfully!")
+        else:
+            print(error_status+"Some thing went wrong...",end=' ErrMsg:'+st["showapi_res_error"]+"\n")
+            #print(st["showapi_res_error"]+"\n")
+            exit(1)
+    except:
+        print(error_status+"Aborted.")
+        exit()
     #print(st)
 main()
