6acfc5c26189627af4e6f03a1380bfe6adebd0fb
NAME <mail@gmail.com>
2022-03-13 20:37:35 +0800

    Change the way to request print url(curl to requests) and add 'print from custom string'

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
 #coding:utf-8
 import getopt,sys,time,os.path
+import os,base64,json
+
+step_status="\033[32m[\033[34m*\033[32m]\033[0m "
+error_status="\033[32m[\033[31m!\033[32m]\033[0m "
+
+try:
+    import requests
+except:
+    print(error_status+"library `request' is needed for printing.\n"+step_status+"Downloading...")
+    os.system("python -m install requests -i https://pypi.tuna.tsinghua.edu.cn/simple")
 global config,printstr,isunix
 isunix=0
 config="Memobird.ini"
 helpdoc=""" usage: python %s [options]\n
  options are:\n
     -h,--help \t\t\tshow this help.
-    -c,--config,--config-file\tset the cofig.
+    -c,--config,--config-file\tset the cofig file.
+    -s,--printstr \t\tprint specified string.
     -f,--file,--print-file\tprint from file.\n
  For more introductions, please see github or readme.
 """%sys.argv[0]
-def read(fname):
+def reqUrl(url):
+    try:
+        print("\033[32m[\033[34m*\033[32m] \033[0mRequesting print url...")
+        r=requests.get(url)
+        #json_s=os.popen("curl -fsS \""+exe+"\"").read() 
+        st=json.loads(r.text)
+        if st["showapi_res_code"]==1:
+            print("\033[32m[\033[34m*\033[32m]\033[0m Print sucessfully!")
+        else:
+            print(error_status+"Some thing went wrong...",end=' ErrMsg:'+st["showapi_res_error"]+"\n")
+            #print(st["showapi_res_error"]+"\n")
+            exit(1)
+    except Exception as e:
+        print(error_status+"Aborted by:")
+        raise e
+        exit()
+
+def readfile(fname):
     fo=open(fname)
     return fo.read()
 def getconf(config="Memobird.ini"):
@@ -38,18 +66,17 @@ def getconf(config="Memobird.ini"):
     exe += devid
     return exe
 
-step_status="\033[32m[\033[34m*\033[32m]\033[0m "
-error_status="\033[32m[\033[31m!\033[32m]\033[0m "
-
 def main(needstr=0,printstr=""):
     global config
-# The base64 encoding depends on linux command 'base64'
+# ~The base64 encoding depends on linux command 'base64'~
+# I'm f**ker because python has encode and base64 lib
     exe=getconf(config)
-    if not needstr :
+    if not needstr:
         unenc=input("\033[32m[\033[34m?\033[32m]\033[0m Enter what you want to print: ")
-    else :
+    elif needstr==2:
+        unenc=readfile(printstr)
+    else:
         unenc=printstr
-    import os,base64,json
     #enced=os.popen("echo \"%s\" > preenc && base64 preenc&&rm preenc"%unenc).read()
     enced=base64.b64encode(unenc.encode('gbk'))
     exe+="&timestamp="+time.strftime("%Y-%m-%d%{}%H:%M:%S".format("%20"),time.localtime())
@@ -66,47 +93,42 @@ def main(needstr=0,printstr=""):
         if flag not in ("y","Y"):
             print(error_status+"Aborted.")
             exit(0)
+        #print(st)
+    # Print now~~
+    reqUrl(exe)
+
+if __name__=="__main__":
+    shortargs = 'f:c:s:h'
+    longargs = ['print-file=','file=','help','printstr='] 
     try:
-        print("\033[32m[\033[34m*\033[32m] \033[0mRequesting print url...")
-        json_s=os.popen("curl -fsS \""+exe+"\"").read() 
-        st=json.loads(json_s)
-        if st["showapi_res_code"]==1:
-            print("\033[32m[\033[34m*\033[32m]\033[0m Print sucessfully!")
-        else:
-            print(error_status+"Some thing went wrong...",end=' ErrMsg:'+st["showapi_res_error"]+"\n")
-            #print(st["showapi_res_error"]+"\n")
-            exit(1)
-    except:
-        print(error_status+"Aborted.")
-        exit()
-    #print(st)
-
-
-shortargs = 'f:c:h'
-longargs = ['print-file=','file=','help'] 
-try:
-    opts,argv=getopt.getopt(sys.argv[1:],shortargs,longargs)
-    #print(opts)
-    if opts==[]:
-        raise TypeError
-    ned=0
-    stri=""
-    for o,a in opts:
-        #print(o,a)
+        opts,argv=getopt.getopt(sys.argv[1:],shortargs,longargs)
         #print(opts)
-        if o in ('-f','--file','--print-file'):
-            #prints=read(a)
-            #print(o,a)
-            ned=1
-            stri=read(a)
-            #cprint(arg)
-        if o in ('--config','--config-file','-c'):
-            #global config
-            config=a
-        elif o in ('-h','--help'):
+        if opts==[] and sys.argv[1]!='':
             print(helpdoc)
             exit()
-    main(ned,stri)
-except (getopt.GetoptError,TypeError):
-    print("\033[32m[\033[34m*\033[32m] \033[0mNo opts detected, using interactive mode.")
-    main()
+        if opts==[]:
+            raise TypeError
+        ned=0
+        stri=""
+        for o,a in opts:
+        #print(o,a)
+        #print(opts)
+            if o in ('-f','--file','--print-file'):
+            #prints=read(a)
+            #print(o,a)
+                ned=2
+                stri=a
+            #cprint(arg)
+            if o in ('--config','--config-file','-c'):
+            #global config
+                config=a
+            if o in ('-s','--printstr'):
+                ned=1
+                stri=a
+            elif o in ('-h','--help'):
+                print(helpdoc)
+                exit()
+        main(ned,stri)
+    except (getopt.GetoptError,TypeError):
+        print("\033[32m[\033[34m*\033[32m] \033[0mNo opts detected, using interactive mode.")
+        main()
